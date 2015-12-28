7ffdc06e971d2ef29adf259278bc0c411779e274
jerry <zhangzhen@700bike.com>
2015-12-28 16:18:49 +0800

    修改包名为qibike

---
+++ src/main/java/com/qibike/thriftnameserver/service/SNodeManagerMBean.java
@@ -0,0 +1,16 @@
+package com.qibike.thriftnameserver.service;
+
+public interface SNodeManagerMBean {
+	String onLine(String serviceName, String host, int port, int pingFrequency);
+
+	String offLine(String serviceName, long id);
+
+	String serviceStatus();
+
+	/**
+	 * help 信息
+	 */
+	String helpOnLine();
+
+	String helpOffline();
+}
