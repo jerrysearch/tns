18a9218974c072345e5ded1025a0a893d2318895
jerry <zhangzhen@700bike.com>
2017-03-30 17:01:58 +0800

    增加example

---
+++ example/src/main/java/com/qibaike/tns/example/ClientExample.java
@@ -0,0 +1,31 @@
+package com.qibaike.tns.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.qibaike.tns.client.client.ServicePool;
+import com.qibaike.tns.client.cluster.ClusterPool;
+import com.qibaike.tns.protocol.rpc.TSNode;
+
+public class ClientExample {
+
+	private static final Logger log = LoggerFactory.getLogger(ClientExample.class);
+	public static void main(String[] args) throws InterruptedException {
+		
+		if(null == args || args.length != 2){
+			log.info("please input server's hostname and service name");
+			return;
+		}
+		
+		ClusterPool clusterPool = new ClusterPool(args[0]);
+		
+		ServicePool servicePool = new ServicePool(clusterPool, args[1], 10);
+		while(true){
+			TSNode node = servicePool.getOne();
+			
+			log.info(node.toString());
+			Thread.sleep(1000L);
+		}
+	}
+
+}
